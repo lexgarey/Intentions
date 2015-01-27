1fb52e551e32750e41a92aa0eca310ee9dd80df8
lexgarey <lexgarey@gmail.com>
2015-01-27 15:30:48 -0800

    Added intention input form/functionality

--- intentions/client/main.js
+++ intentions/client/main.js
@@ -1,7 +1,26 @@
 if (Meteor.isClient) {
   Template.body.helpers({
     intentions: function () {
-      return Intentions.find({});
+      return Intentions.find({}, { $set: {createdAt: -1}});
     }  
+  });
+
+  Template.body.events({
+    "submit .new-intention": function () {
+      // Called when the new intention form is submitted
+
+      var text = event.target.text.value;
+
+      Intentions.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      // Clears form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
   });
 }
